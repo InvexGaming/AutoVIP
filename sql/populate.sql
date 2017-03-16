6ca7628188535e77d40687358ac6ac0a72397182
Mohammad Ghasembeigi <me@mohammadg.com>
2017-03-17 07:23:14 +1100

    Remove unneeded client variable pass to callback

--- sql/populate.sql
+++ sql/populate.sql
@@ -4,14 +4,14 @@
 USE autovip;
 
 -- CSGO Servers
-INSERT INTO servers (name)
-VALUES  ('invexdev'),
-        ('combatsurf'),
-        ('surfdm'),
-        ('jailbreak'),
-        ('1v1'),
-        ('retake'),
-        ('retake2');
+INSERT INTO servers (short_name, nice_name, active)
+VALUES  ('invexdev', 'Invex Dev', true),
+        ('combatsurf', 'Combat Surf', true),
+        ('surfdm', 'Surf DM', true),
+        ('jailbreak', 'Jailbreak', true),
+        ('1v1', '1v1', true),
+        ('retake', 'Retakes', true),
+        ('retake2', 'Retakes HS', true);
         
 -- Permissions
 INSERT INTO permissions (tag)
@@ -25,17 +25,18 @@ VALUES  ('@staff'),
  
  
 -- Packages
-INSERT INTO packages (name, description, cost, credits_included, num_servers, duration, active, permissionid)
-VALUES  ('General', 'General/Misc donations', 0.00, 0, -1, 0, false, NULL),
-        ('Staff', 'Given to all Staff', 0.00, 0, 0, 0, true, 1),
-        ('Global', 'Given to all Globals', 0.00, 0, 0, 0, true, 2),
-        ('Server Admin', 'Given to all Server Admins', 0.00, 0, 1, 0, true, 3),
-        ('Veteran', 'Given to all Veterans', 0.00, 0, 0, 0, true, 4),
-        ('Credits', 'Basic credit package', 1.00, 800, 1, 0, false, NULL),
-        ('Package 1', 'Reservation slot', 5.00, 0, 1, 30, true, 5),
-        ('Package 2', 'Premium VIP', 10.00, 5000, 1, 30, true, 6),
-        ('Package 3', 'Premium VIP', 27.00, 5000, 3, 60, true, 6),
-        ('Package 4', 'Premium VIP', 50.00, 5000, 0, 90, true, 6),
-        ('Package 5', 'Permanent VIP', 65.00, 0, 0, 0, true, 7),
-        ('Package 6', 'Permanent VIP', 200.00, 0, 0, 0, true, 7);
-        
+INSERT INTO packages (name, description, cost, credits_included, num_servers, duration, active, permissionid, ordernum)
+VALUES  ('General', 'General/Misc donations', 0.00, 0, -1, 0, false, NULL, 99999),
+        ('Staff', 'Given to all Staff', 0.00, 0, 0, 0, true, 1, 1),
+        ('Global', 'Given to all Globals', 0.00, 0, 0, 0, true, 2, 2),
+        ('Server Admin', 'Given to all Server Admins', 0.00, 0, 1, 0, true, 3, 3),
+        ('Veteran', 'Given to all Veterans', 0.00, 0, 0, 0, true, 4, 4),
+        ('Credits', 'Basic credit package', 1.00, 800, 1, 0, false, NULL, 6),
+        ('Package 1', 'Reservation slot', 5.00, 0, 1, 30, true, 5, 7),
+        ('Package 2', 'Premium VIP', 10.00, 5000, 1, 30, true, 6, 8),
+        ('Package 3', 'Premium VIP', 27.00, 5000, 3, 60, true, 6, 9),
+        ('Package 4', 'Premium VIP', 50.00, 5000, 0, 90, true, 6, 10),
+        ('Package 5', 'Permanent VIP', 65.00, 0, 1, 0, true, 7, 11),
+        ('Package 6', 'Permanent VIP', 200.00, 0, 0, 0, true, 7, 12),
+        ('Jailbreak POTM', 'Given to monthly POTM winners', 0.00, 2500, 1, 30, true, 6, 5),
+        ('Top10 Free VIP', 'Top10 ranked rewards', 0.00, 2500, 1, 30, true, 6, 5);
--- sql/setup.sql
+++ sql/setup.sql
@@ -12,7 +12,9 @@ DROP TABLE IF EXISTS servers;
 CREATE TABLE servers
 (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(64), -- name of server (nice name)
+  short_name VARCHAR(64), -- short name, no spaces, all lowercase
+  nice_name VARCHAR(64), -- nice name
+  active BIT(1) NOT NULL, -- is server active or not
   PRIMARY KEY (id)
 );
 
@@ -42,6 +44,7 @@ CREATE TABLE packages
   duration INT NOT NULL,  -- in days, 0 for permanent
   active BIT(1) NOT NULL, -- inactive packages not added to admin_simple.ini file
   permissionid INT,
+  ordernum INT DEFAULT 99999, -- used to determine which order packages are displayed
   FOREIGN KEY (permissionid) REFERENCES permissions(id),
   PRIMARY KEY (id)
 );
@@ -56,10 +59,11 @@ CREATE TABLE transactions
   cost DECIMAL(13, 4) NOT NULL, -- cumulative/total amount paid in transaction
   steamid VARCHAR(32), -- prefixed STEAM_1:Y:Z
   username VARCHAR(64),
-  userid INT,
+  userid INT, -- invalid or missing = -1
   start_date INT(11) NOT NULL,
   end_date INT(11), -- NULL if permanent
   packageid INT,
+  comment VARCHAR(255), -- for extra info
   PRIMARY KEY(id),
   FOREIGN KEY (packageid) REFERENCES packages(id)
 );
